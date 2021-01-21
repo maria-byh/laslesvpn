2fc25b8c7581dc9f8653b3cbd7f29a8c3f50f721
maria2001-byh <59501786+maria2001-byh@users.noreply.github.com>
2021-01-21 16:10:11 +0100

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        cg: '#4f5665',
+        cr: '#f53838',
+        cb: '#0b132a',
+        cn: '#2fab73',
+      },
+      fontFamily: {
+        rubik: 'Rubik',
+      },
+      boxShadow: {
+        sr: '0 25px 50px -12px #f53838', 
+        sg: '0px 0px 20px rgba(0, 0, 0, 0.1)',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
